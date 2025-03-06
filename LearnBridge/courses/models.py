e7698e9f4d3dbd86ea3bb074e5fef1e171f6a4cf
agodahdev Goddah <godahahmed@gmail.com>
2025-03-06 23:10:21 +0000

    Create course model. Closes #5

--- LearnBridge/courses/models.py
+++ LearnBridge/courses/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=200)  # Course title
+    description = models.TextField()  # Course description
+    price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)  # Course price
+    date_created = models.DateTimeField(auto_now_add=True)  # Auto timestamp when created
+
+    def __str__(self):
+        return self.title
