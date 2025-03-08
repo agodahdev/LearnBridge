01709a56d6e29e32ee2e97644a42e685a9b32f5b
agodahdev Goddah <godahahmed@gmail.com>
2025-03-08 10:36:03 +0000

    added test for user

--- LearnBridge/users/tests.py
+++ LearnBridge/users/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+# Test if a user can register
+class UserModelTest(TestCase):
+    def test_create_user(self):
+        user = User.objects.create_user(username="testuser", password="testpass123")
+        self.assertEqual(user.username, "testuser")
