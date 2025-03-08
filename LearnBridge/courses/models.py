2084d184537dfd2e23bd2378670f56b8eaba095b
agodahdev Goddah <godahahmed@gmail.com>
2025-03-08 10:16:14 +0000

    added enrollement model. close #6

--- LearnBridge/courses/models.py
+++ LearnBridge/courses/models.py
@@ -2,11 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Course(models.Model):
-    title = models.CharField(max_length=200)  # Course title
+    title = models.CharField(max_length=255)  # Course title
     description = models.TextField()  # Course description
     price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)  # Course price
     date_created = models.DateTimeField(auto_now_add=True)  # Auto timestamp when created
 
     def __str__(self):
-        return self.title
-    
+        return f"{self.user.username} enrolled in {self.course.title}"
+
+# Enrollment Model
+class Enrollment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    enrolled_at = models.DateTimeField(auto_now_add=True)
