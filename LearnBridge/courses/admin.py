c856037fad8ecf372a2fcc295c24982ea54d5e41
agodahdev Goddah <godahahmed@gmail.com>
2025-03-08 10:41:15 +0000

    added admin to site access

--- LearnBridge/courses/admin.py
+++ LearnBridge/courses/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Course
+
 # Register your models here.
 admin.site.register(Course)
+admin.site.register(Enrollment)
+admin.site.register(Review)
