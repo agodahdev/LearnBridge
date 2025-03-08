224d55510921af6ebcfcaa5a42d74acae53d4022
agodahdev Goddah <godahahmed@gmail.com>
2025-03-08 10:45:02 +0000

    added test in courses app

--- LearnBridge/courses/tests.py
+++ LearnBridge/courses/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Course
 
-# Create your tests here.
+# Test if a course is created correctly
+class CourseModelTest(TestCase):
+    def test_create_course(self):
+        course = Course.objects.create(
+            title="Test Course",
+            description="This is a test description.",
+            price=100.00
+        )
+        self.assertEqual(course.title, "Test Course")
