cfa71f369632f6262247bd1f5ba2d582cb81e926
agodahdev Goddah <godahahmed@gmail.com>
2025-03-08 10:25:38 +0000

    added form for submitting course reviews

--- LearnBridge/courses/forms.py
+++ LearnBridge/courses/forms.py
@@ -1,7 +1,14 @@
 from django import forms
-from .models import Course
+from .models import Course, Review
 
+# Form for course creation and updates
 class CourseForm(forms.ModelForm):
     class Meta:
         model = Course
         fields = ['title', 'description', 'price']
+
+# Form for submitting course reviews
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ['rating', 'comment']
