a3f461cc98dab18df26ed6b6803882da04605f02
agodahdev Goddah <godahahmed@gmail.com>
2025-03-08 01:18:04 +0000

    create course detail page

--- LearnBridge/courses/views.py
+++ LearnBridge/courses/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
-from .models import Course
-# Create your views here.
-def course_list(request):
-    courses = Course.objects.all()
-    return render(request, 'courses/course_list.html', {'courses': courses})
+from django.shortcuts import get_object_or_404
+from .models import Enrollment
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
+# Allows logged-in users to enroll in a course
+@login_required
+def enroll(request, course_id):
+    course = get_object_or_404(Course, id=course_id)
+    Enrollment.objects.create(user=request.user, course=course)
+    messages.success(request, f"You have enrolled in {course.title}!")
+    return redirect("course_list")
+
+# View for showing detailed course info
+def course_detail(request, course_id):
+    course = get_object_or_404(Course, id=course_id)
+    return render(request, "courses/course_detail.html", {"course": course})
