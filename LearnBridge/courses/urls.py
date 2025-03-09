c98c25fc6df548e57795472172ccc2a71a15c801
agodahdev Goddah <godahahmed@gmail.com>
2025-03-09 09:31:47 +0000

    fixed view course details

--- LearnBridge/courses/urls.py
+++ LearnBridge/courses/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.course_list, name='course_list'),  # View all courses
     path('course/<int:course_id>/', views.course_detail, name='course_detail'),  # View course details
+    path('course/<int:course_id>/review/', views.submit_review, name='submit_review'),
     path('course/create/', views.create_course, name='create_course'),  # ✅ Create a course
     path('course/update/<int:course_id>/', views.update_course, name='update_course'),  #  Update a course
     path('course/delete/<int:course_id>/', views.delete_course, name='delete_course'),  # Delete a course
